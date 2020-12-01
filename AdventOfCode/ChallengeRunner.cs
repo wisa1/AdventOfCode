110b72a6901c283db3385f12a5e4bca3207abe60
Johannes Naderer <j.naderer@countit.at>
2020-12-01 18:32:55 +0100

    day 1 solutions

---
+++ AdventOfCode/ChallengeRunner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    class ChallengeRunner
+    {
+        public static void Main(string[] args)
+        {
+            //AdventOfCode.Day1.Challenge1.Day1Challenge1();
+            //AdventOfCode.Day1.Challenge2.Day1Challenge2();
+        }
+    }
+}
