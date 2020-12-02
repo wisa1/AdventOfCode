b3d3ce572b2c57528bb5da26cb6cda9883406065
Johannes Naderer <j.naderer@countit.at>
2020-12-02 18:24:06 +0100

    created ChallengeRunner

--- AdventOfCode/ChallengeRunner.cs
+++ AdventOfCode/ChallengeRunner.cs
@@ -8,8 +8,10 @@ namespace AdventOfCode
     {
         public static void Main(string[] args)
         {
-            //AdventOfCode.Day1.Challenge1.Day1Challenge1();
-            //AdventOfCode.Day1.Challenge2.Day1Challenge2();
+            // AdventOfCode.Day1.Challenge1.Day1Challenge1();
+            // AdventOfCode.Day1.Challenge2.Day1Challenge2();
+            // AdventOfCode.Day2.Challenge1.Day2Challenge1();
+            AdventOfCode.Day2.Challenge2.Day2Challenge2();
         }
     }
 }
