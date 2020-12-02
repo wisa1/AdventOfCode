5f637600d5202f16f3c19421679ec524b3410eb5
Johannes Naderer <j.naderer@countit.at>
2020-12-02 18:24:23 +0100

    completed Day2

---
+++ AdventOfCode/Day2/Challenge1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace AdventOfCode.Day2
+{
+    class Challenge1
+    {
+        public static void Day2Challenge1()
+        {
+            List<PasswordPolicy> passwordPolicies = InitTestData();
+            int validPasswordCount = passwordPolicies.Where(x => x.IsValid()).ToList().Count;
+            Console.WriteLine(validPasswordCount);
+        }
+
+        private static List<PasswordPolicy> InitTestData()
+        {
+            List<PasswordPolicy> policies = new List<PasswordPolicy>();
+            string fileContent = File.ReadAllText(@"C:\Users\Wisa\source\repos\AdventDay1\AdventOfCode\Day2\Day1Input.txt");
+
+            var arr = fileContent.Split('\n');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string MinMax = arr[i].Split(":")[0];
+                int Min = Convert.ToInt32(MinMax.Split("-")[0]);
+                int Max = Convert.ToInt32(MinMax.Split("-")[1].Split(" ")[0]);
+                char Character = Convert.ToChar(MinMax.Split("-")[1].Split(" ")[1]);
+                string Password = arr[i].Split(":")[1].Trim();
+
+                policies.Add(new PasswordPolicy
+                {
+                    MinimumAllowed = Min,
+                    MaximumAllowed = Max,
+                    Character = Character,
+                    Password = Password
+                });
+            }
+            return policies;
+        }
+    }
+}
---
+++ AdventOfCode/Day2/PasswordPolicy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Day2
+{
+    class PasswordPolicy
+    {
+        public int MinimumAllowed{ get; set; }
+        public int MaximumAllowed { get; set; }
+        public char Character { set; get; }
+        public string Password { set; get; }
+        public int PasswordCharacterCount { set; get; } = 0;
+        
+        // Challenge 1 
+        public bool IsValid()
+        {
+            for (int i = 0; i < Password.Length; i++)
+            {
+                if (Password[i] == Character)
+                    PasswordCharacterCount++;
+            }
+
+            if (PasswordCharacterCount >= MinimumAllowed && PasswordCharacterCount <= MaximumAllowed)
+                return true;
+            return false;
+        }
+
+        // Challenge 2
+        public bool IsValid2()
+        {
+            char first  = Password[MinimumAllowed - 1];
+            char second = Password[MaximumAllowed - 1];
+
+            if (first == Character ^ second == Character)
+                return true;
+            return false;
+        }
+    }    
+}
